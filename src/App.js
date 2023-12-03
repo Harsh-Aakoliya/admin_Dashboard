f5d8fbabe540ba770ef4af6378ccfd4a6bde2752
Harsh Aakoliya <harshaakoliya20@gmail.com>
2023-12-03 18:36:20 +0530

    changed project name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Admin Panel</h1>
+        <h1>Admin Dashboard</h1>
       </header>
       <main>
         <AdminPanel />
